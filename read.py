fdc0b368a4cc4f8e4897e2bb9003adb0dbe97cfa
asd953114 <asd953114@gmail.com>
2019-01-29 17:13:29 +0800

    calc avg len of reciews

--- read.py
+++ read.py
@@ -3,8 +3,13 @@ count = 0
 with open("reviews.txt", "r") as f:
 	for line in f:
 		data.append(line)
-		count += 1
+		count += 1 
 		if count % 1000 == 0:
 			print(len(data))
 
-print(len(data))
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+	print(sum_len)
+print("讀取完了,總共",len(data),"筆資料")
+print("留言的平均長度為", sum_len / len(data))
